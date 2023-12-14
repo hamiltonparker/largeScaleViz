7121a9f280db00f5664decbee90db6f4e484be47
Parker Hamilton <prkr.hamilton@gmail.com>
2023-12-13 22:16:14 -0800

    Added I/O function to read from lammps dump file Added animation loop to iterate over dataset Added I/O functio to output frames as PPM files

--- src/atomv.c
+++ src/atomv.c
@@ -6,7 +6,11 @@
 ***********************************************************************/
 #include "atomv.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include "mpi.h"
+#define GL_SILENCE_DEPRECATION
 #include <OpenGL/gl.h>    /* Header file for the OpenGL library */
 #include <OpenGL/glu.h>   /* Header file for the GLu library */
 #include <GLUT/glut.h>    /* Header file for the GLut library */
@@ -17,6 +21,8 @@ GLdouble fovy, aspect, near_clip, far_clip;
                           /* parameters for gluPerspective() */
 FILE *fp;                 /* pointer to open an MD-configuration file */
 
+int nprocs, myid;
+
 /* Function prototypes ************************************************/
 void reshape(int, int);
 void makeFastNiceSphere(GLuint, double);
@@ -26,6 +32,10 @@ void drawScene(void);
 void display(void);
 void initView(float *, float *);
 void readConf(void);
+void animate(void);
+void writeFrame(void);
+void PPMWriter(unsigned char *, char*, int, int);
+void setStepRange(int, int);
 
 /**********************************************************************/
 void reshape (int w, int h) {
@@ -118,14 +128,21 @@ void makeAtoms() {
   Makes display-list of all atoms in the current frame using spheres.
 ***********************************************************************/
   int i;
-  float rval,gval,bval;
+  float rval1,gval1,bval1;
+  float rval2,gval2,bval2;
 
   glNewList(atomsid, GL_COMPILE);
-  rval = Ratom; gval = Gatom; bval = Batom;  /* RGB color of an atom */
+  rval1 = Ratom_1; gval1 = Gatom_1; bval1 = Batom_1;  /* RGB color of an atom */
+  rval2 = Ratom_2; gval2 = Gatom_2; bval2 = Batom_2;
   for (i=0; i < natoms; i++) {
     glPushMatrix();
     glTranslatef(atoms[i].crd[0],atoms[i].crd[1],atoms[i].crd[2]);
-    glColor3f(rval,gval,bval);
+    if (atomType[i] == 1){
+      glColor3f(rval1,gval1,bval1);
+    }
+    else if (atomType[i] == 2){
+      glColor3f(rval2,gval2,bval2);
+    }
     glCallList(sphereid);
     glPopMatrix();
   }
@@ -172,17 +189,22 @@ void initView (float *min_ext, float *max_ext) {
   Initializes global viewing, lighting, and projection values.
 ***********************************************************************/
   GLfloat light_diffuse[]   = {1.0, 1.0, 1.0, 1.0};
-  GLfloat light_position1[] = {0.5, 0.5, 1.0, 0.0};
+  GLfloat light_position1[] = {0.3, 0.5, 1.0, 0.0};
+  GLfloat light_specular[]  = {1.0, 1.0, 1.0, 1.0};
+  GLfloat light_position2[] = {0.3, 0.5, 1.0, 0.0};
   float dif_ext[3],dis;
   int i;
 
   /* Define normal light */
   glLightfv(GL_LIGHT0,GL_DIFFUSE,light_diffuse);
   glLightfv(GL_LIGHT0,GL_POSITION,light_position1);
+  glLightfv(GL_LIGHT1,GL_SPECULAR,light_specular);
+  glLightfv(GL_LIGHT1,GL_POSITION,light_position2);
 
   /* Enable a single OpenGL light */
   glEnable(GL_LIGHTING);
   glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHT1);
 
   /* Use depth buffering for hidden surface elimination */
   glEnable(GL_DEPTH_TEST);
@@ -197,16 +219,16 @@ void initView (float *min_ext, float *max_ext) {
   for (i=0; i<3; i++) center[i] = min_ext[i] + dif_ext[i]/2.0;
 
   /* set initial eye & look at location in world space */
-  eye[0] = center[0];
+  eye[0] = center[0] - dis*0.25*0.6;
   eye[1] = center[1];
-  eye[2] = center[2] + dis;
-  up[0] = 0.0;
-  up[1] = 1.0;
-  up[2] = 0.0;
+  eye[2] = center[2] + dis*0.4*0.6;
+  up[0] = 1.0;
+  up[1] = 0.0;
+  up[2] = 1.0;
 
   /* set parameters for gluPerspective() */
   /* Near- & far clip-plane distances */
-  near_clip = (GLdouble)( 0.5*(dis-0.5*dif_ext[2]) );
+  near_clip = (GLdouble)( 0.1*(dis-0.5*dif_ext[2]) );
   far_clip  = (GLdouble)( 2.0*(dis+0.5*dif_ext[2]) );
   /* Field of view */
   fovy = (GLdouble)( 0.5*dif_ext[1]/(dis-0.5*dif_ext[2]) );
@@ -240,14 +262,117 @@ necessary arrays.
   fclose(fp);
 }
 
+void readDump(char* pattern) {
+/*
+Read atomic coordinates from lammps dump file
+*/
+  int l, j;
+  int isAtoms = 0;
+  char line[MAX_LINE_LENGTH] = {0};
+  FILE * file = fopen(pattern,"r");
+
+  if (file == NULL) {
+    printf("Damn");
+  }
+
+  while (fgets(line, MAX_LINE_LENGTH, file)){
+      if (strstr(line, "ITEM: BOX BOUNDS") != NULL) {
+        for (l=0; l<3; l++) fscanf(file,"%f%f",&min_ext[l],&max_ext[l]);
+        continue;
+      }
+      if (strstr(line, "ITEM: NUMBER OF ATOMS") != NULL){
+        fscanf(file,"%d",&natoms);
+        continue;
+      }
+      if (strstr(line, "ITEM: ATOMS") != NULL){
+        break;
+      }
+  }
+
+  atoms = (AtomType *) malloc(sizeof(AtomType)*natoms);
+  atomType = (int *) malloc(sizeof(int)*natoms);
+  for (j=0; j<natoms; j++) {
+    fgets(line, MAX_LINE_LENGTH, file);
+    sscanf(line, "%*d %d %f %f %f %*f %*f %*f ",&(atomType[j]), &(atoms[j].crd[0]),
+                                &(atoms[j].crd[1]),&(atoms[j].crd[2]));
+  }
+  fclose(fp);
+
+}
+
+void animate() {
+
+  if (step < stepLim) {
+    char snum[12] = {0};
+    sprintf(snum, "%d", step);
+    char fPat[strlen(basePat)+10];
+    strcpy(fPat,basePat);
+    strcat(fPat,snum);
+    readDump(fPat);
+    makeCurframeGeom();
+    glutPostRedisplay();
+    writeFrame();
+  }
+
+  step = step + stepSize;
+
+}
+
+void writeFrame() {
+  unsigned char *image = (unsigned char*) malloc(sizeof(unsigned char)*3*winx*winy);
+  glReadPixels(0, 0, winx, winy, GL_RGB, GL_UNSIGNED_BYTE, image);
+
+  char buffer [33];
+  sprintf(buffer,"../frames/img_frame_%d.ppm",step);
+  PPMWriter(image,buffer,winx,winy);
+}
+
+void PPMWriter(unsigned char *in, char *name, int dimx, int dimy) {
+  int i, j;
+  FILE *file = fopen(name,"w");
+  (void) fprintf(file, "P3 \n%d %d \n255\n",dimx, dimy);
+  
+  size_t tmp;
+
+  for (j = 0; j < dimy; j++) {
+    for (i = 0; i < dimx; i++) {
+      static unsigned char color[3];
+      color[0] = in[3*i+3*(dimy-j-1)*dimy];
+      color[1] = in[3*i+3*(dimy-j-1)*dimy+1];
+      color[2] = in[3*i+3*(dimy-j-1)*dimy+2];
+      fprintf(file, "%d %d %d ", color[0],color[1],color[2]);
+    }
+    fprintf(file,"\n");
+  }
+  (void) fclose(file);
+}
+
+void setStepRange(int nprocs, int myid) {
+  int stepRange = stepLim - step;
+  int nSteps = stepRange/stepSize/nprocs;
+
+  step = myid * nSteps;
+  if (myid != nprocs-1){
+    stepLim = nSteps*(myid + 1);
+  }
+
+}
+
 /**********************************************************************/
 int main(int argc, char **argv) {
 /**********************************************************************/
 
+  MPI_Init(&argc, &argv);
+  
   glutInit(&argc, argv);
 
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+  setStepRange(nprocs,myid);
+
   /* Read atomic coordinates from an MD-configuration file */
-  readConf();
+  readDump("/Users/parkerh/Documents/research/USC/MXenes/dump/run_Ti3C2_450_270_40/dump.Ti3C2_450_270_4.0.0");
 
   /* Set up an window */
   /* Initialize display mode */
@@ -263,6 +388,7 @@ int main(int argc, char **argv) {
   /* Set a glut callback functions */
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
+  glutIdleFunc(animate);
 
   /* generate an OpenGL display list for single sphere */
   sphereid = glGenLists(1);
@@ -276,6 +402,7 @@ int main(int argc, char **argv) {
   /* Start main display loop */
   glutMainLoop();
   
+  MPI_Finalize();
   return 0;
 }
 /**********************************************************************/
--- src/atomv.h
+++ src/atomv.h
@@ -1,9 +1,13 @@
 /***********************************************************************
   atomv.h: an include file for atom.c
 ***********************************************************************/
-#define Ratom 1.0         /* RGB color of an atom */
-#define Gatom 0.0
-#define Batom 0.0
+#define Ratom_1 1.0         /* RGB color of an atom */
+#define Gatom_1 0.2
+#define Batom_1 0.2
+#define Ratom_2 0.2
+#define Gatom_2 0.2
+#define Batom_2 1.0
+#define MAX_LINE_LENGTH 100
 
 typedef struct {          /* Atom data type */
   float crd[3];
@@ -11,13 +15,23 @@ typedef struct {          /* Atom data type */
 
 int nlon=18, nlat=9;      /* Number of polygons for a sphere in the 
                              longitudinal & lateral directions */
-float atom_radius = 0.2;  /* Atomic radius in Lennard-Jones unit */
-int winx=640, winy=640;   /* Window size */
+float atom_radius = 0.9;  /* Atomic radius in Lennard-Jones unit */
+int winx=1280, winy=1280;   /* Window size */
 float min_ext[3], max_ext[3];  
                           /* Range of atomic coordinates:
                              (left,lower,back), (right,top,front) */
 int natoms;               /* number of atoms */
 AtomType *atoms;          /* array of atoms */
+int *atomType;
 float eye[3];             /* position of eye point */
 float center[3];          /* position of look reference point */
 float up[3];              /* up direction for camera */
+
+/**********************************************************************
+Parameters to loop through the appropriate simulation files to animate
+**********************************************************************/
+char *basePat = "/Users/parkerh/Documents/research/USC/MXenes/dump/run_Ti3C2_450_270_40/dump.Ti3C2_450_270_4.0.";
+int stepLim = 9000;
+int stepSize = 1000;
+int step = 0;
+
